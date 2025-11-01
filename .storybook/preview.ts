425958496b12f34e599ebac3a5e4a102ec6ccad7
samhalsall23 <54165031+samhalsall23@users.noreply.github.com>
2025-11-01 12:45:31 +0000

    feat(storybook and link): add storybook and first link component

---
+++ .storybook/preview.ts
@@ -0,0 +1,40 @@
+import type { Preview } from "@storybook/nextjs-vite";
+import "../src/app/globals.css";
+import "./storybook.css";
+
+const preview: Preview = {
+  parameters: {
+    backgrounds: {
+      default: "light",
+      values: [
+        {
+          name: "light",
+          value: "#ffffff",
+        },
+        {
+          name: "dark",
+          value: "#333333",
+        },
+        {
+          name: "gray",
+          value: "#f5f5f5",
+        },
+      ],
+    },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+
+    a11y: {
+      // 'todo' - show a11y violations in the test UI only
+      // 'error' - fail CI on a11y violations
+      // 'off' - skip a11y checks entirely
+      test: "todo",
+    },
+  },
+};
+
+export default preview;
