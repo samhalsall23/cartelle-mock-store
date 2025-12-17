f4a940c3143283b7e1da008f3af1082501342414
samhalsall23 <samhalsall23@gmail.com>
2025-12-17 19:16:37 +1100

    feat(section hero): delay animation until image loads

--- src/components/common/HeroSection/HeroSection.tsx
+++ src/components/common/HeroSection/HeroSection.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import Image from "next/image";
+import { useState } from "react";
 
 import { BaseSection } from "@/components/layout";
 import { AnimateFadeIn } from "@/components/ui";
@@ -6,6 +9,9 @@ import { cn } from "@/lib";
 import { HeroSectionButton } from "./HeroSectionButton";
 
 export function HeroSection() {
+  // === STATE ===
+  const [imageLoaded, setImageLoaded] = useState(false);
+
   return (
     <BaseSection
       id="hero-image"
@@ -19,8 +25,13 @@ export function HeroSection() {
           quality={80}
           priority
           className="object-cover rounded-sm"
+          onLoad={() => setImageLoaded(true)}
         />
-        <AnimateFadeIn disableIsInView={true} duration="long">
+        <AnimateFadeIn
+          key={imageLoaded ? "loaded-gradient" : "loading-gradient"}
+          disableIsInView={!imageLoaded}
+          duration="long"
+        >
           <div
             className={cn(
               "absolute inset-0 rounded-sm",
@@ -30,7 +41,11 @@ export function HeroSection() {
             )}
           />
         </AnimateFadeIn>
-        <AnimateFadeIn disableIsInView={true} duration="long">
+        <AnimateFadeIn
+          key={imageLoaded ? "loaded-content" : "loading-content"}
+          disableIsInView={!imageLoaded}
+          duration="long"
+        >
           <div className="absolute inset-0 flex flex-col xl:flex-row justify-end items-center xl:items-end px-6 py-10 md:p-10 xl:p-12 text-white">
             <HeroSectionButton className="order-2 xl:order-1 mt-6 xl:mt-0" />
             <div className="flex flex-col xl:text-end gap-4 xl:ms-auto order-1 xl:order-2">
