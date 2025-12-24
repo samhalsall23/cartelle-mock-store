bd514505d6d19b89d17c5be494b6015cf0bd470f
samhalsall23 <54165031+samhalsall23@users.noreply.github.com>
2025-12-24 04:51:40 +0000

    feat(component newsletter card): add newsletter card component

---
+++ src/components/common/NewsletterCard/NewsletterCard.stories.tsx
@@ -0,0 +1,30 @@
+import type { Meta, StoryObj } from "@storybook/nextjs-vite";
+
+import { NewsletterCard } from "./NewsletterCard";
+
+const meta = {
+  title: "Base/NewsletterCard",
+  component: NewsletterCard,
+  parameters: {
+    layout: "centered",
+    docs: {
+      description: {
+        component:
+          "A newsletter card component that encourages users to subscribe to email updates, displaying a title, description, email input, sign-up button, and an accompanying image.",
+      },
+    },
+  },
+  tags: ["autodocs"],
+  argTypes: {},
+} satisfies Meta<typeof NewsletterCard>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  render: () => (
+    <div className="p-5 bg-neutral-10">
+      <NewsletterCard />
+    </div>
+  ),
+};
---
+++ src/components/common/NewsletterCard/NewsletterCard.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+
+import { Button, Input } from "@/components/ui";
+
+export function NewsletterCard() {
+  return (
+    <div className="bg-white border rounded-sm p-6 md:p-8 xl:p-10 flex flex-col xl:flex-row gap-10">
+      <div className="flex flex-col gap-12 justify-between xl:justify-around order-2 xl:order-1">
+        <h3 className="text-2xl xl:text-3xl">
+          Stay Ahead with Exclusive Deals!
+        </h3>
+        <p className="text-neutral-10 text-base font-medium gap-12 max-w-150">
+          {
+            "Be the first to know about special offers, new product drops, and insider updates. Join our newsletter and get exclusive perks delivered straight to your inbox!"
+          }
+        </p>
+        <div className="flex flex-col md:flex-row w-full gap-2">
+          <Input
+            className="flex-1 w-full md:w-auto"
+            variant="light"
+            id="newsletter"
+            type="email"
+            placeholder="Enter your email"
+          />
+          <Button
+            className="w-full md:w-auto"
+            variant="light"
+            text="Sign up to newsletter"
+          />
+        </div>
+      </div>
+      <div className="relative xl:w-1/2 aspect-video order-1 xl:order-2">
+        <Image
+          src="/assets/hero-3.jpg"
+          alt="Newsletter"
+          fill
+          className="object-cover rounded-sm"
+          sizes="(max-width: 1280px) 50vw, 100vw"
+        />
+      </div>
+    </div>
+  );
+}
