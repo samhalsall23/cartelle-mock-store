3de23d9b6cc8eaf2615de4127fc5c3540a3bddc8
Sam Halsall <samhalsall23@gmail.com>
2026-01-24 21:55:03 +1100

    feat(prisma schema): add cascade on delete

---
+++ prisma/migrations/20260123064354_add_cascade_deletes/migration.sql
@@ -0,0 +1,17 @@
+-- DropForeignKey
+ALTER TABLE "CartItem" DROP CONSTRAINT "CartItem_productId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "CartItem" DROP CONSTRAINT "CartItem_sizeId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Size" DROP CONSTRAINT "Size_productId_fkey";
+
+-- AddForeignKey
+ALTER TABLE "Size" ADD CONSTRAINT "Size_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CartItem" ADD CONSTRAINT "CartItem_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CartItem" ADD CONSTRAINT "CartItem_sizeId_fkey" FOREIGN KEY ("sizeId") REFERENCES "Size"("id") ON DELETE CASCADE ON UPDATE CASCADE;
