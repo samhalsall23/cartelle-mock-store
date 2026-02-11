01096d553bedd1026148a4a9dd50783efee6ae47
Sam Halsall <samhalsall23@gmail.com>
2026-02-11 22:22:47 +1100

    fix(support image): change to webp

--- src/app/(customerFacing)/support/page.tsx
+++ src/app/(customerFacing)/support/page.tsx
@@ -37,7 +37,7 @@ export default function SupportPage() {
             <Image
               priority
               className="object-cover rounded-sm"
-              src="/assets/support-hero.jpg"
+              src="/assets/support-hero.webp"
               alt="Support Image"
               sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
               quality={60}
